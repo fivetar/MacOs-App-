a5d42aaa66f4e6ad6c0bc3d6dbc862130cb342b9
fivetar <122467816+fivetar@users.noreply.github.com>
2024-10-02 09:33:07 -0500

    Update MacOsApp.swift

--- MacOsApp.swift
+++ MacOsApp.swift
@@ -1,1 +1,16 @@
+# October 2, 2024
+Just starting learning how to code for MacOs with swift
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe") 
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
 
